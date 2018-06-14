6c7bcc15f10d7e3802220bd48e4c4a3010b2bd4d
“Ricardo <riicardoxd_9@hotmail.com>
2018-06-14 18:34:16 -0500

    Creacion de pagina nosotros.hbs con parciales, la clase active del navbar funciona para la url en la que nos encontremos

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require( 'express' );
 const app = express();
+const hbs = require( 'hbs' );
 const PORT = 3000;
 
 app.use( express.static( __dirname + '/public' ) );
 
+hbs.registerPartials( __dirname + '/views/parciales' );
 // MOTOR DE VISTA
 app.set( 'view engine', 'hbs' );
 
 app
     .get( '/', (req, res) => {
-
        res.render( 'index.hbs', {
            nombre: 'Jose Abdiel', 
            anio: new Date().getFullYear()
         } );
     } )
     .get( '/contacto', (req, res) => {
-        res.render( 'contacto.hbs' );
+        res.render( 'contacto.hbs', {
+            active_contacto: true,
+            anio: new Date().getFullYear()
+        } );
+     } )
+     .get( '/nosotros', (req, res) => {
+        res.render( 'nosotros.hbs', {
+            anio: new Date().getFullYear()
+        } );
      } )
     .listen( PORT, () => { 
         console.log( 'Servidor escuchando en el puerto: ' + PORT )
